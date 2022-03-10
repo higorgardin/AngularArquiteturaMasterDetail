3cbf1d5611d6da8703a8877713ab4bf21d77fe01
Higor Gardin <higorgardin00@gmail.com>
2022-03-09 22:52:24 -0300

    Criado componente para listagem de receitas

---
+++ finansys/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Entry } from '../shared/entry.model';
+import { EntryService } from '../shared/entry.service';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css'],
+})
+export class EntryListComponent implements OnInit {
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) {}
+
+  ngOnInit() {
+    this.entryService.getAll().subscribe(
+      entries => (this.entries = entries),
+      error => alert('Erro ao buscar categorias')
+    );
+  }
+
+  deleteEntry(entry: Entry) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete)
+      this.entryService.delete(entry.id).subscribe(
+        () => (this.entries = this.entries.filter(element => element !== entry)),
+        error => alert('Erro ao excluir categoria')
+      );
+  }
+}
