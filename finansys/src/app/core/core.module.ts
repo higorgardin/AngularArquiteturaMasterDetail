c7b3ebfd7d644d23becc8ded515a202e647ca4bc
Higor Gardin <higorgardin00@gmail.com>
2022-04-04 23:09:09 -0300

    Reestruturação do projeto em módulos

---
+++ finansys/src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from '../in-memory-database';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+  ],
+  exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+})
+export class CoreModule {}
--- finansys/src/app/pages/categories/categories.module.ts
+++ finansys/src/app/pages/categories/categories.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryService } from './shared/category.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
-  imports: [CommonModule, CategoriesRoutingModule, ReactiveFormsModule],
+  imports: [SharedModule, CategoriesRoutingModule],
   providers: [CategoryService],
 })
 export class CategoriesModule {}
