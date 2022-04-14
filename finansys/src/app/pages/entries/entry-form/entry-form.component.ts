6fbe8c8016e6b1947d10074cef3d54175c3c83d1
Higor Gardin <higorgardin00@gmail.com>
2022-04-13 23:31:47 -0300

    Melhorado baseResourceService, entryService e categoryService

--- finansys/src/app/pages/entries/entry-form/entry-form.component.ts
+++ finansys/src/app/pages/entries/entry-form/entry-form.component.ts
@@ -152,7 +152,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private createEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
 
     this.entryService.create(entry).subscribe(
       entry => this.actionsForSuccess(entry),
@@ -161,7 +161,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private updateEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
 
     this.entryService.update(entry).subscribe(
       entry => this.actionsForSuccess(entry),
