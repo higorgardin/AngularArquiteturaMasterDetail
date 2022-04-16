e3bc520a812da2cbdcf2c566fb6e7acf288e3bce
Higor Gardin <higorgardin00@gmail.com>
2022-04-16 14:38:22 -0300

    Extraĩdo componente de navbar

--- finansys/src/app/core/core.module.ts
+++ finansys/src/app/core/core.module.ts
@@ -7,16 +7,19 @@ import { CommonModule } from '@angular/common';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule,
   ],
-  exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, NavbarComponent],
 })
 export class CoreModule {}
