3c1f7a891223ac3bdeebcd55a16c76b87371cbd9
Higor Gardin <higorgardin00@gmail.com>
2022-04-15 23:19:02 -0300

    Criado component de page-header

--- finansys/src/app/shared/shared.module.ts
+++ finansys/src/app/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  exports: [CommonModule, ReactiveFormsModule, RouterModule, BreadCrumbComponent],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    BreadCrumbComponent,
+    PageHeaderComponent,
+  ],
 })
 export class SharedModule {}
